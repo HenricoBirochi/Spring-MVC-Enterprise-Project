783dab330f327365d0e34945c7be30f54899c0ad
HenricoBirochi <henrico.birochi@gmail.com>
2025-10-06 14:27:02 -0300

    feat: DepartamentoService and FuncionarioService added with them interfaces classes

--- src/main/java/henrico/empresa_mvc/service/interfaces/DepartamentoService.java
+++ src/main/java/henrico/empresa_mvc/service/interfaces/DepartamentoService.java
@@ -1,5 +1,18 @@
 package henrico.empresa_mvc.service.interfaces;
 
+import java.util.List;
+
+import henrico.empresa_mvc.model.Departamento;
+
 public interface DepartamentoService {
-    
+
+    void salvar(Departamento departamento);
+
+    void editar(Departamento departamento);
+
+    void excluir(Long id);
+
+    Departamento buscarPorId(Long id);
+
+    List<Departamento> buscarTodos();
 }
--- src/main/java/henrico/empresa_mvc/service/interfaces/FuncionarioService.java
+++ src/main/java/henrico/empresa_mvc/service/interfaces/FuncionarioService.java
@@ -1,5 +1,18 @@
 package henrico.empresa_mvc.service.interfaces;
 
+import java.util.List;
+
+import henrico.empresa_mvc.model.Funcionario;
+
 public interface FuncionarioService {
-    
+
+    void salvar(Funcionario funcionario);
+
+    void editar(Funcionario funcionario);
+
+    void excluir(Long id);
+
+    Funcionario buscarPorId(Long id);
+
+    List<Funcionario> buscarTodos();
 }
