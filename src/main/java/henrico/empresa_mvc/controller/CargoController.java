3ad120b802506d10bcaca164752bed5c406b09a7
Henrico Birochi <henrico.birochi@gmail.com>
2025-10-08 08:35:29 -0300

    fix: Refactoring the name of the packages of the controllers to support the changing on the structure of the folders

--- src/main/java/henrico/empresa_mvc/controller/CargoController.java
+++ src/main/java/henrico/empresa_mvc/controller/CargoController.java
@@ -1,4 +1,4 @@
-package henrico.empresa_mvc.web.controller;
+package henrico.empresa_mvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
