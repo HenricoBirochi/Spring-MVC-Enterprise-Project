cecbdef7ca8d4ebcd2d3ff08ea420c36ccb85fd2
HenricoBirochi <henrico.birochi@gmail.com>
2025-10-08 17:41:26 -0300

    feat: Connecting templates(Views) with controllers and DB in the form view of 'departamento'

--- src/main/java/henrico/empresa_mvc/controller/DepartamentoController.java
+++ src/main/java/henrico/empresa_mvc/controller/DepartamentoController.java
@@ -1,21 +1,37 @@
 package henrico.empresa_mvc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import henrico.empresa_mvc.model.Departamento;
+import henrico.empresa_mvc.service.interfaces.DepartamentoService;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
 
-    @GetMapping("/cadastrar")
-    public String cadastrar() {
-        return "/departamento/cadastro";
+    @Autowired
+    private DepartamentoService service;
+
+    @GetMapping("/cadastrar")// Quando vc coloca um objeto no parametro do metodo, ele vai para a view (pro template)
+    public String cadastrar(Departamento departamento) {
+        return "departamento/cadastro";
     }
 
     @GetMapping("/listar")
     public String listar() {
-        return "/departamento/lista";
+        return "departamento/lista";
     }
+
+    @PostMapping("/salvar")
+    public String salvar(Departamento departamento) {
+        service.salvar(departamento);
+        return "redirect:/departamentos/cadastrar";
+    }
+
 }
